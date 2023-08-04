bd2a50d9183d6f8b20beca745d124019d3a30154
Harold Mbarga <mbah00@hotmail.com>
2023-08-04 12:32:07 -0400

    updated values files with new images

--- expresso-Shop-app/expresso.sh
+++ expresso-Shop-app/expresso.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 
+
 helm install my-expresso-shop-product ./expresso-shop-product
-helm install my-expresso-shop-review ./my-expresso-shop-review
+helm install my-expresso-shop-review ./expresso-shop-review
 helm install my-expresso-shop-web ./expresso-shop-web
 helm install my-postgresql-ha ./postgresql-ha
 helm install my-redis-ha ./redis-ha
 helm install my-expresso-Shop-app ./expresso-Shop-app
 
+Kubectl get all
